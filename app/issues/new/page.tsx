99a36a6532f1086ba5e96addddc62de7530ae7b5
Mahshad Mollazamani <mmolazamani@gmail.com>
2024-01-29 13:37:37 +0100

    Disable SSR for loading Markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,22 +1,25 @@
 'use client'
-import { Button, Callout, TextField, TextFieldInput } from '@radix-ui/themes'
-import SimpleMDE from "react-simplemde-editor";
-import { useForm, Controller } from 'react-hook-form';
-import axios from 'axios';
-import "easymde/dist/easymde.min.css";
-import React, { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { createIssueSchema } from '@/app/validationSchemas';
-import { z } from 'zod';
-import { Text } from '@radix-ui/themes';
 import ErrorMessage from '@/app/components/ErrorMessage';
 import Spinner from '@/app/components/Spinner';
+import { createIssueSchema } from '@/app/validationSchemas';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button, Callout, TextField, TextFieldInput } from '@radix-ui/themes';
+import axios from 'axios';
+import "easymde/dist/easymde.min.css";
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { z } from 'zod';
 
 /* interface IssueForm {
   title: string;
   description: string;
 } */
+
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'),
+ { ssr: false }
+ )
 
 type IssueForm = z.infer<typeof createIssueSchema>;
  
