71e362920e0c7699246ed41a734b8eaccc03dc1f
Mahshad Mollazamani <mmolazamani@gmail.com>
2024-02-15 14:42:09 +0100

    Refactoring the Asignee Select Components

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,53 +1,37 @@
 'use client';
 
 import { Issue, User } from '@prisma/client';
-import { Select } from '@radix-ui/themes';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import {Skeleton} from '@/app/components';
 import toast, { Toaster } from 'react-hot-toast'
+import { Select } from '@radix-ui/themes';
 
 
-const AssigneeSelect = ({ issue }: { issue: Issue} ) => {
-  const {data: users, error, isLoading } = useQuery<User[]>({
-    queryKey: ['users'],
-    queryFn: () => axios.get('/api/users').then(res => res.data),
-    staleTime: 60*1000, //60s
-    retry: 3
-  });
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
+  const { data: users, error, isLoading } = useUsers();
 
   if (isLoading) return <Skeleton />
 
-  if(error) return null;
+  if (error) return null;
 
-/*   const [users, setUsers] = useState<User[]>([]);
-
-  useEffect( () => { 
-    const fetchUsers = async() => {
-      const {data} = await axios.get<User[]>('/api/users');
-      setUsers(data);
-    } 
-    fetchUsers();
-    
-  }, []) */
-
+  const assignIssue = (value: string) => {
+    const payload = { assignedToUserId: value === 'unassigned' ? null : value };
+    axios
+      .patch('/api/issues/' + issue.id, payload)
+      .catch(() => {
+        toast.error('Changes could not be saved.')
+      });
+  } 
+  
 
   return (
-   /* { <Select.Root onValueChange={(userId) => {
-      axios.patch('/api/issues/' + issue.id, { assignedToUserId: userId || null });
-    }}> }*/
     <>
       <Select.Root 
         defaultValue={issue.assignedToUserId || "unassigned"}
-        onValueChange={(value) => {
-        const payload = { assignedToUserId: value === 'unassigned' ? null : value };
-        axios
-          .patch('/api/issues/' + issue.id, payload)
-          .catch(() => {
-            toast.error('Changes could not be saved.')
-          });
-      }}>
+        onValueChange={assignIssue}
+      >
 
           <Select.Trigger placeholder='Assign...' />
           <Select.Content>
@@ -63,5 +47,14 @@ const AssigneeSelect = ({ issue }: { issue: Issue} ) => {
     </>
   )
 }
+  
 
-export default AssigneeSelect
+  const useUsers = () => useQuery<User[]>({
+    queryKey: ['users'],
+    queryFn: () => axios.get('/api/users').then(res => res.data),
+    staleTime: 60*1000, //60s
+    retry: 3
+  });
+
+
+export default AssigneeSelect;
