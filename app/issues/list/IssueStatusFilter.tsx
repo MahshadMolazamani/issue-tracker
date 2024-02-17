13c465ab2264575bf8e861a13d037d5ebf626911
Mahshad Mollazamani <mmolazamani@gmail.com>
2024-02-17 17:26:10 +0100

    Fix filtering box

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -1,38 +1,48 @@
-'use client';
+"use client";
 
-import { Status } from '@prisma/client';
-import { Select } from '@radix-ui/themes'
-import { useRouter } from 'next/navigation';
-import React from 'react';
+import { Status } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import { useRouter, useSearchParams } from "next/navigation";
+import React from "react";
 
-const statuses: { label: string, value?: Status }[] = [
-    {label: 'Open', value: 'OPEN'}, 
-    {label: 'In Progress', value: 'IN_PROGRESS'}, 
-    {label: 'Closed', value: 'CLOSED'}, 
-]
+const statuses: { label: string; value?: Status }[] = [
+  { label: "Open", value: "OPEN" },
+  { label: "In Progress", value: "IN_PROGRESS" },
+  { label: "Closed", value: "CLOSED" },
+];
 
 const IssueStatusFilter = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
-    const router = useRouter(); 
   return (
-    <Select.Root onValueChange={(status) => {
+    <Select.Root
+      defaultValue={searchParams.get("status") || ""}
+      onValueChange={(status) => {
+        const params = new URLSearchParams();
+        if (status) params.append("status", status);
+        if (searchParams.get("orderBy"))
+          params.append("orderBy", searchParams.get("orderBy")!);
         /* const query = status ? `?status=${status}` : '';
         router.push('/issues/list' + query) */
-        const query = status && status !== 'unassigned' ? `?status=${status}` : '';
-        router.push('/issues/list' + query)
-    }}>
-        <Select.Trigger placeholder='Filter by status...' />
-        <Select.Content>
-            <Select.Item key="unassigned" value="unassigned">
-                All
-            </Select.Item> 
-            { statuses.map( status => (
-                <Select.Item key={status.value} value={status.value || 'unknown'}>
-                    {status.label}
-                </Select.Item>))}
-        </Select.Content>
+        //const query = status && status !== 'unassigned' ? `?status=${status}` : '';
+        const query = params.toString();
+        router.push("/issues/list?" + query);
+      }}
+    >
+      <Select.Trigger placeholder="Filter by status..." />
+      <Select.Content>
+        <Select.Item key="unassigned" value="unassigned">
+          All
+        </Select.Item>
+        {statuses.map((status) => (
+          <Select.Item key={status.value} value={status.value || "unknown"}>
+            {status.label}
+          </Select.Item>
+        ))}
+      </Select.Content>
     </Select.Root>
-  )
-}
+  );
+};
 
-export default IssueStatusFilter
+export default IssueStatusFilter;
